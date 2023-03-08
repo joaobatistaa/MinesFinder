6d3cc5c3c393d66e1939e9d7fd93a59222d38595
João Batista <joaopedrobatista.73@outlook.com>
2023-03-08 12:24:40 +0000

    Versão com Layout Concluido

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MinesFinder extends JFrame {
     private JPanel painelPrincipal;
@@ -6,6 +8,16 @@ public class MinesFinder extends JFrame {
     private JLabel labelTitulo;
     private JPanel painelCentro;
     private JLabel labelRecordes;
+    private JLabel labelNivelMedio;
+    private JLabel labelNomeJogador1;
+    private JLabel labelNivelFacil;
+    private JLabel labelNomeJogador2;
+    private JLabel labelNivelDificil;
+    private JLabel labelNomeJogador3;
+    private JButton botaoJogoMedio;
+    private JButton botaoJogoDificil;
+    private JButton botaoSair;
+    private JButton botaoJogoFacil;
 
     public MinesFinder(String title) {
         super(title);
@@ -13,8 +25,19 @@ public class MinesFinder extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(painelPrincipal);
         pack();
+        botaoSair.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        botaoSair.addActionListener(this::btnSairActionPerformed);
     }
     public static void main(String[] args) {
         new MinesFinder("Mines Finder").setVisible(true);
+    }
+
+    private void btnSairActionPerformed(ActionEvent e) {
+        System.exit(0);
     }
 }
